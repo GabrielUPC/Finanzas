fa7b64bae6e4fb5492626d89ab9b3fb3bc881397
JoseLuis <u202219637@upc.edu.pe>
2025-05-27 15:27:58 -0500

    Archivo base

---
+++ src/main/java/pe/edu/upc/tf_finanzas2025/TfFinanzas2025Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.tf_finanzas2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfFinanzas2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfFinanzas2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/tf_finanzas2025/TfFinanzas2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.tf_finanzas2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfFinanzas2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
